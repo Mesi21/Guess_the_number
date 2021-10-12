193ebae3e8923578a095e9660a0a0627474d64e9
Mesi <mse212002@gmail.com>
2021-10-12 11:13:45 +0300

    :construction: started adding game logic

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 'use strict';
+const secretNum = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNum;
 
-document.querySelector('.message');
+document.querySelector('.check').addEventListener('click', () => {
+  const guess = Number(document.querySelector('.guess').value);
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No number!';
+  } else if (guess === secretNum) {
+    document.querySelector('.message').textContent = 'Correct number!';
+  } else if (guess > secretNum) {
+    document.querySelector('.message').textContent = 'Too high!';
+  } else {
+    document.querySelector('.message').textContent = 'Too low!';
+  }
+})
